9002e4463ad9e84497bffbaa2b44e02b086b4f6d
黑猫警长 <847738646@qq.com>
2025-03-25 20:58:21 +0800

    Fix Microsoft Clarity import and initialization

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import ReactGA from 'react-ga4';
-import { clarity } from '@microsoft/clarity';
+import Clarity from '@microsoft/clarity';
 
 // u521du59cbu5316 Google Analytics
 ReactGA.initialize('G-QEXE5CEM9R');
 
 // u521du59cbu5316 Microsoft Clarity
-clarity.init('qtqq4gf289');
+Clarity.init('qtqq4gf289');
 
 // 
 import './assets/styles/global.less';
